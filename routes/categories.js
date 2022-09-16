1ff6517bb31ee44c26ea62694995a32a1e2c15b5
Kashil <kashilrr@gamil.com>
2022-09-16 16:17:36 +0530

    16/09/2022 commit

--- routes/categories.js
+++ routes/categories.js
@@ -33,4 +33,31 @@ router.post("/", async (req, res) => {
     res.send(category);
 });
 
+// Update
+router.put("/:id", async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id, {
+        name: req.body.name,
+        icon: req.body.icon || category.icon,
+        color: req.body.color,
+    },
+        { new: true }
+    );
+    if (!category) return res.status(400).send("the category cannot be created");
+    res.send(category);
+});
+
+// Delete
+router.delete("/:id", (req, res) => {
+    Category.findByIdAndRemove(req.params.id)
+        .then((category) => {
+            if (category) {
+                return res.status(200).json({ success: true, message: "the category is deleted" });
+            }
+            else {
+                return res.status(500).json({ success: false, error: err });
+            }
+        });
+});
+
 module.exports = router;
