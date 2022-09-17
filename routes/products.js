9851b3e1f1a53e54933fb3187668bf0ee4aca52f
Kashil <kashilrr@gamil.com>
2022-09-17 15:21:12 +0530

    17/09/2022 commit

--- routes/products.js
+++ routes/products.js
@@ -13,9 +13,18 @@ router.get('/', async (req, res) => {
 
 router.post('/',(req,res)=>{
     const product = new Product({
-        name:req.body.name,
-        image:req.body.image,
-        countInStock:req.body.countInStock.type,
+        name:req.body.name, 
+        description:req.body.description, 
+        richDescription:req.body.richDescription, 
+        image:req.body.image, 
+        images:req.body.images, 
+        brand:req.body.brand, 
+        price:req.body.price, 
+        // category : req.body.category, 
+        countInStock:req.body.countInStock, 
+        rating:req.body.rating, 
+        numReviews:req.body.numReviews, 
+        isFeatured:req.body.isFeatured 
     });
 
     console.log(product)
@@ -32,5 +41,57 @@ router.post('/',(req,res)=>{
             });
         });
 });
+
+//put 
+router.put("/:id",async(req,res)=>{ 
+    // if(!mongoose.isValidObjectId(req.params.id)){ 
+    //     return res.status(400).send("Invalid Product Id") 
+    // // } 
+    // let category=await Category.findById(req.body.category) 
+    // if(!category) return res.status(400).send("Invalid Category") 
+ 
+    let product=await Product.findByIdAndUpdate( 
+      req.params.id, 
+      {  
+        name : req.body.name, 
+        description : req.body.description, 
+        richDescription : req.body.richDescription, 
+        image : req.body.image, 
+        images : req.body.images, 
+        brand : req.body.brand, 
+        price : req.body.price, 
+        // category : req.body.category,    
+        countInStock : req.body.countInStock, 
+        rating : req.body.rating, 
+        numReviews : req.body.numReviews, 
+        isFeatured : req.body.isFeatured,    
+      }, 
+      {new:true} 
+    ); 
+    if (!product) return res.status(400).send("the category cannot be created.."); 
+    res.send (product); 
+  }); 
+ 
+ 
+  //delete 
+  router.delete("/:id",async (req,res)=>{ 
+    Product.findByIdAndRemove(req.params.id) 
+    .then((category)=>{ 
+        if(category){ 
+            return res 
+            .status (200) 
+            .json({success:true,message:"The category is deleted"}) 
+        } 
+        else{ 
+            return res 
+            .status(404) 
+            .json({success: false, message:"category not found"}) 
+        } 
+    })  
+    .catch((err)=>{ 
+        return res.status(500).json({success: false, error: err}) 
+    }); 
+}); 
+ 
 
 module.exports = router;
